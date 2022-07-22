a813d9ec7cf7581ee5d444d0ab9cbbd25b852a49
KevinKaras <kevinkaras2@gmail.com>
2022-07-22 19:33:00 -0400

    added notes to backend routes for later

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,28 @@ app.config.from_object(Config)
 db.init_app(app)
 Migrate(app, db)
 
+# HOME ROUTE
+@app.route('/')
+def Home():
+
+    # GRAB ALL SCORES
+    # GRAB ALL USERS 
+    # FOR LEADERBOARD
+    user = User.query.all()
+    return "<div>NOT BAD</div>"
+
 
 @app.route('/')
 def Home():
-    
+    # 
     user = User.query.all()
-    print(user)
+    return "<div>NOT BAD</div>"
+
+# ROUTE TO UPDATE WINS FOR USER ID
+@app.route('/win/<int:id>')
+def Winner(id):
+    # GRAB USER BY ID
+    # UPDATE HIS/HERS WINS BY 1
+    # RETURN USER:ID, SCORE OF USERID
+    user = User.query.all()
     return "<div>NOT BAD</div>"
