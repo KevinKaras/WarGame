cb7205a6da034401a2d01e75de634e78e2e8a6f6
KevinKaras <kevinkaras2@gmail.com>
2022-07-23 19:55:04 -0400

    added frontend end layout just colors

---
+++ react-app/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <div className='Left-Side'>
+        <div className='User-Bar'>
+          {/* <SignUp /> */}
+        </div>
+        <div className="Game">
+          {/* <Deck1 /> */}
+          {/* <CardDisplay /> */}
+          {/* <Deck2 /> */}
+        </div>
+      </div>
+      <div className='Right-Side'>
+        <div className='LeaderBoard'>
+          {/* <Leaderboard /> */}
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
