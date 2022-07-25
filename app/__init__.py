779db6cf3e678fd9bf17e71586abe704f0a5e761
KevinKaras <kevinkaras2@gmail.com>
2022-07-25 11:24:55 -0400

    state of the signup area is complete for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from app.config import Config
+from flask_cors import CORS
 from .models import db, Score
 from flask_migrate import Migrate
 from .forms.SignUpForm import SignUpForm
 
-print(SignUpForm)
-print(Flask)
+# print(Migrate)
+# print(Flask)
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 Migrate(app, db)
+CORS(app)
+
 
 # HOME ROUTE
 @app.route('/')
@@ -22,24 +25,31 @@ def Home():
     scores = Score.query.all()
     return {"score": score.to_dict() for score in scores}
 
-@app.route('/SignUp')
+@app.route('/signup', methods=["POST"])
 def SignUp():
     # GRAB INFO FROM REQUEST
     # CREATE SIGNUPFORM WITH INFO FROM REQUEST
     # MAKE NEW ADDITION TO DATABASE, COMMIT CHANGES
     form = SignUpForm()
+    print(form.data)
+    newScore1 = Score(
+        name = form.data["name1"],
+        wins = 0
+    )
+    newScore2 = Score(
+        name = form.data["name2"],
+        wins = 0
+    )
+
+    db.session.add_all([newScore1,newScore2])
+    db.session.commit()
+    scores = {
+        newScore1,
+        newScore2
+    }
     
-    
-    return 
+    return {"Scores": score.to_dict() for score in scores}
 
-
-# @app.route('/')
-# def Home():
-#     # 
-#     user = User.query.all()
-#     return "<div>NOT BAD</div>"
-
-# ROUTE TO UPDATE WINS FOR USER ID
 @app.route('/win/<int:id>')
 def Winner(id):
     # GRAB USER BY ID
--- react-app/src/components/SignUp.js
+++ react-app/src/components/SignUp.js
@@ -2,14 +2,13 @@ import './CSS/SignUp.css'
 import { useEffect, useState } from "react";
 function SignUp() {
 
-    let [name1, setName1] = useState('')
-    let [name2, setName2] = useState('')
+    let [name1, setName1] = useState('Player 1 Name')
+    let [name2, setName2] = useState('Player 2 Name')
     let [winAmt, setWinAmt] = useState('')
-
+    let [signedUpState, setSignedUpState] = useState(false)
 
     const onCreate = async (event) => {
         event.preventDefault()
-
         let formData = new FormData()
 
         formData.append("name1", name1)
@@ -19,36 +18,56 @@ function SignUp() {
             method: "POST",
             body: formData
         })
+        setSignedUpState(true)
+        console.log(signedUpState, "STATE AFTER CHANGE")
         
     }
+    useEffect(()=> {
+
+    }, [])
 
     return (
-        <div className="Sign-Up-Container">
-            <form className="Player-Name-Form" onSubmit={onCreate}>
-                <div className="Player-1-Input">
-                    <input 
-                    className="Player-1-Name"
-                    type="text"
-                    name="name"
-                    placeholder="Player 1 Name"
-                    onChange={(e) => setName1(e.target.value)}
-                    ></input>
+        <div>
+        { !signedUpState &&
+            <div className="Sign-Up-Container">
+                <form className="Player-Name-Form" onSubmit={onCreate}>
+                    <div className="Player-1-Input">
+                        <input 
+                        className="Player-1-Name"
+                        type="text"
+                        name="name"
+                        placeholder={name1}
+                        onChange={(e) => setName1(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className="Player-2-Input">
+                        <input 
+                        className="Player-2-Name"
+                        type="text"
+                        name="name"
+                        placeholder={name2}
+                        onChange={(e) => setName2(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className="Button-Container">
+                        <button className="Sign-Up-Button" type="Submit">Start Game</button>
+                    </div>
+                </form>
+                
+            </div>
+        }
+        { signedUpState && 
+            <div className="Current-Players">
+                <div className="Player-1-Display">
+                    <div className='Player-1-Display-Name'>{name1}</div>
                 </div>
-                <div className="Player-2-Input">
-                    <input 
-                    className="Player-2-Name"
-                    type="text"
-                    name="name"
-                    placeholder="Player 1 Name"
-                    onChange={(e) => setName2(e.target.value)}
-                    ></input>
+                <div className="Player-2-Display">
+                    <div className='Player-2-Display-Name'>{name2}</div>   
                 </div>
-                <div className="Button-Container">
-                    <button className="Sign-Up-Button" type="Submit">Start Game</button>
-                </div>
-            </form>
-            
+            </div>
+        }
         </div>
+        
     );
   }
   
