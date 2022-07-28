0ee6d9cf326667993ec791eeb3737bcdd163b895
KevinKaras <kevinkaras2@gmail.com>
2022-07-28 17:44:43 -0400

    added main route to render react on /

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
+from flask import Flask, render_template, request, session, redirect
 from .config import Config
 from flask_cors import CORS
 from .models import db, Score
+from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_migrate import Migrate
 from .forms.SignUpForm import SignUpForm
 
@@ -70,3 +73,32 @@ def Winner(id):
 
 
     return "<div>NOT BAD</div>"
+
+# @app.before_request
+# def https_redirect():
+#     if os.environ.get('FLASK_ENV') == 'production':
+#         if request.headers.get('X-Forwarded-Proto') == 'http':
+#             url = request.url.replace('http://', 'https://', 1)
+#             code = 301
+#             return redirect(url, code=code)
+
+
+# @app.after_request
+# def inject_csrf_token(response):
+#     response.set_cookie('csrf_token',
+#                         generate_csrf(),
+#                         secure=True if os.environ.get(
+#                             'FLASK_ENV') == 'production' else False,
+#                         samesite='Strict' if os.environ.get(
+#                             'FLASK_ENV') == 'production' else None,
+#                         httponly=True)
+#     return response
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
