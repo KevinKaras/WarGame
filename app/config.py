30735cfc1349f56260aa27ce937cea610b2862de
KevinKaras <kevinkaras2@gmail.com>
2022-07-28 20:29:16 -0400

    added dialect changer again

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
-# .replace("://", "ql://", 1)
+
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS=False
